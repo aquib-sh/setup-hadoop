f8255c90151b034983aca6a1200a043e0f9eb6dc
Aquib <shaikhaquib394@gmail.com>
2022-11-20 03:11:11 -0500

    Download and unzip only if not already

--- setup_hadoop.sh
+++ setup_hadoop.sh
@@ -6,20 +6,30 @@ function print_task_completion() {
 }
 
 echo "[+] Downloading Java"
-wget https://download.java.net/java/GA/jdk19.0.1/afdd2e245b014143b62ccb916125e3ce/10/GPL/openjdk-19.0.1_linux-x64_bin.tar.gz
+if !test -f "hadoop-3.3.4.tar.gz"; then
+    wget https://download.java.net/java/GA/jdk19.0.1/afdd2e245b014143b62ccb916125e3ce/10/GPL/openjdk-19.0.1_linux-x64_bin.tar.gz
+fi
 print_task_completion
 
 echo "[+] Downloading Hadoop"
-wget https://dlcdn.apache.org/hadoop/common/hadoop-3.3.4/hadoop-3.3.4.tar.gz
+if !test -f "openjdk-19.0.1_linux-x64_bin.tar.gz"; then
+    wget https://dlcdn.apache.org/hadoop/common/hadoop-3.3.4/hadoop-3.3.4.tar.gz
+fi
 print_task_completion
 
 echo "[+] Extracting files"
-tar -xvzf hadoop-3.3.4.tar.gz
-tar -xvzf openjdk-19.0.1_linux-x64_bin.tar.gz
+if !test -d "hadoop-3.3.4"; then
+    tar -xvzf hadoop-3.3.4.tar.gz
+fi
+
+if !test -d "jdk-19.0.1"; then
+    tar -xvzf openjdk-19.0.1_linux-x64_bin.tar.gz
+fi
 print_task_completion
 
-if [! -f /home/$USER/.bashrc ]; then
-    touch /home/$USER/.bashrc
+BASH_PROFILE=/home/$USER/.bashrc
+if !test -f $BASH_PROFILE; then
+    touch $BASH_PROFILE
 fi
 
 echo "[+] Setting up environment variables"
